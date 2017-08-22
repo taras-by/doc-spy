7fb706b5b49d1bcc39ac6e92d66254452a556e7a
Taras <landgradus@gmail.com>
2017-08-22 16:28:20 +0300

    Add new Command parser:run for parsing document using URL

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Debril\RssAtomBundle\DebrilRssAtomBundle(),
             new AppBundle\AppBundle(),
             new ParserBundle\ParserBundle(),
         ];
---
+++ src/ParserBundle/Command/ParserRunCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ParserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ParserRunCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('parser:run')
+            ->setDescription('Parse document')
+            ->addArgument('url', InputArgument::REQUIRED, 'Document URL')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $url = $input->getArgument('url');
+
+        $feedIo = $this->getContainer()->get('feedio');
+        $feed = $feedIo->read($url)->getFeed();
+        
+        foreach ($feed as $item) {
+            $output->writeln($item->getTitle());
+        }
+    }
+}
