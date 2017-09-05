0acf27320f8eb11c57c0bde58938cdb22ac9f4e6
Taras <landgradus@gmail.com>
2017-09-05 10:30:53 +0300

    Temporary fix with out --no-dev

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 git checkout .
 git checkout master
 git pull origin master --force
-composer install --no-dev --optimize-autoloader
+composer install --optimize-autoloader
 php bin/console cache:clear --env=prod --no-debug --no-warmup
 php bin/console cache:warmup --env=prod
