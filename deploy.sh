630120fecf7c03d095a57aec2b651ddbaeb96b12
Taras <landgradus@gmail.com>
2018-05-14 23:02:53 +0300

    Change the paramters in the deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git checkout .
 git checkout master
 git pull origin master --force
-composer install --no-dev --optimize-autoloader
+composer install --optimize-autoloader
 php bin/console cache:clear --env=prod --no-debug
 #php bin/console assetic:dump --env=prod --no-debug
 php bin/console doctrine:schema:update --force
