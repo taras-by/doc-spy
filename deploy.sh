2dcdccb80860e74fd3bdd24eda8f016948dbb2ec
Taras <landgradus@gmail.com>
2018-05-14 21:58:55 +0300

    Common Post-Deployment Tasks

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 git checkout .
 git checkout master
 git pull origin master --force
-composer install --optimize-autoloader
-php bin/console cache:clear --env=prod --no-debug --no-warmup
-php bin/console cache:warmup --env=prod
+composer install --no-dev --optimize-autoloader
+php bin/console cache:clear --env=prod --no-debug
+#php bin/console assetic:dump --env=prod --no-debug
+php bin/console doctrine:schema:update --force
